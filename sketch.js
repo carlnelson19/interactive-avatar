658c5666b9de1beaea2d2d1fb988b6480b754a4d
carlnelson19 <carlnelson19@gmail.com>
2017-02-20 12:28:36 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,77 @@
 function setup() {
-
+createCanvas(1200,600);
+background (255);
+    var words = [ "1", "2", "3", "4" ];
+var word = random(words);   
 }
 
 function draw() {
+
+//var words = [ "1", "2", "3", "4" ];
+//var word = random(words);
+    
+    //left rectangle
+    if(mouseX < 600){
+        noStroke();
+        fill(233, 255, 0);
+        rect(0, 0, 600, 600);
+        stroke(0);
+        noFill();
+        textSize(200);
+        text("back", mouseX-200, mouseY);
+        
+        noStroke();
+        fill(0);
+        rect(600, 0, 600, 600); 
+    }
+        if(mouseIsPressed &&
+        mouseX < 600){
+        noStroke();
+        fill(0);
+        rect(0, 0, 600, 600);
+        stroke(233, 255, 0);
+        noFill();
+        textSize(200);
+        text("back", mouseX-200, mouseY);  
+       
+        noStroke();
+        fill(233, 255, 0);
+        rect(600, 0, 600, 600);    
+        
+    }   
+    
+    
+    //right rectangle
+        
+    if (mouseX > 600){
+        noStroke();
+        fill(0);
+        rect(600, 0, 600, 600);
+        stroke(233, 255, 0);
+        noFill();
+        textSize(200);
+        text("forth", mouseX-200, mouseY)
+        
+        noStroke();
+        fill(233, 255, 0);
+        rect(0, 0, 600, 600);
+    }
+    if (mouseIsPressed &&
+            mouseX > 600){
+            noStroke();
+            fill(233, 255, 0);
+            rect(600, 0, 600, 600);
+            stroke(0);
+            noFill();
+            textSize(200);
+            text("forth", mouseX-200, mouseY); 
+        
+        noStroke();
+        fill(0);
+        rect(0, 0, 600, 600);
+    }
+ 
+
+  
 
 }
